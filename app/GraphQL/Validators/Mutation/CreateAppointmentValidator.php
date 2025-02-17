adf173d77af05b5a81aeaa25eced592731ada5a1
Irakli Shevardenidze <i.shevardenidze@optimogroup.io>
2025-02-17 11:48:27 +0400

    added ka validator messages to create appointment mutation

--- app/GraphQL/Validators/Mutation/CreateAppointmentValidator.php
+++ app/GraphQL/Validators/Mutation/CreateAppointmentValidator.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Validators\Mutation;
 
+use Illuminate\Support\Facades\App;
 use Nuwave\Lighthouse\Validation\Validator;
 
 final class CreateAppointmentValidator extends Validator
@@ -48,8 +49,35 @@ final class CreateAppointmentValidator extends Validator
      */
     public function messages(): array
     {
-        return [
-            'phone.regex' => 'The phone number must be in one of the formats: xxxxxxxxx, +995xxxxxxxxx, or 032xxxxxxx.',
+        $locale = App::getLocale();
+
+        $messages = [
+            'ka' => [
+                'name' => [
+                    'required' => 'სახელი აუცილებელია',
+                    'string' => 'სახელი უნდა იყოს სტრიქონი',
+                    'min' => 'სახელი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან',
+                    'max' => 'სახელი უნდა შედგებოდეს მაქსიუმუმ 255 სიმბოლოსგან',
+                ],
+                'surname' => [
+                    'required' => 'გვარი აუცილებელია',
+                    'string' => 'გვარი უნდა იყოს სტრიქონი',
+                    'min' => 'გვარი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან',
+                    'max' => 'გვარი უნდა შედგებოდეს მაქსიუმუმ 255 სიმბოლოსგან',
+                ],
+                'specialty_id' => [
+                    'exists' => 'სპეციალობა მითითებული აიდით არ მოიძებნა'
+                ],
+                'doctor_id' => [
+                    'exists' => 'ექიმი მითითებული აიდით არ მოიძებნა'
+                ],
+                'phone' => [
+                    'required' => 'ტელეფონის ნომერი აუცილებელია',
+                    'regex' => 'ნომრის ფორმატი უნდა ემთხვეოდეს რომელიმეს: xxxxxxxxx, +995xxxxxxxxx, ან 032xxxxxxx.',
+                ]
+            ]
         ];
+
+        return $messages[$locale];
     }
 }
